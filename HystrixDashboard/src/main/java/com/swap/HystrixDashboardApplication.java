f1aef01e1f311366faf5221dcb01e1ebc896a788
Swapnil Nyayade <swapnilnyayade040@gmail.com>
2022-11-12 16:49:45 +0530

    microservicesV2

--- HystrixDashboard/src/main/java/com/swap/HystrixDashboardApplication.java
+++ HystrixDashboard/src/main/java/com/swap/HystrixDashboardApplication.java
@@ -2,9 +2,11 @@ package com.swap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableHystrixDashboard
 public class HystrixDashboardApplication {
 
